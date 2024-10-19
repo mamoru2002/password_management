41c4cf0584ca6cb1865ac994f1d1aa9765c148f7
mamoru2002 <tiusika.m@gmail.com>
2024-10-19 12:03:38 +0000

    Get Passwordの追加

--- password_management.sh
+++ password_management.sh
@@ -4,7 +4,7 @@ ACCOUNT="password_management_table"
 
 while true; do
 	echo "パスワードマネージャへようこそ！"
-	echo "次の選択肢から入力してください(Add Password)："
+	echo "次の選択肢から入力してください(Add Password/Get Password)："
 	read option
 
 	case $option in
@@ -18,5 +18,25 @@ while true; do
 			echo "$service:$user:$password" >> $ACCOUNT
 			echo "パスワードの追加は成功しました。"
 			;;
+		"Get Password")
+			echo "サービス名を入力してください："
+			read service
+			
+			#入力されたサービス名からアカウントを検索
+			result=$(grep "^$service:" $ACCOUNT)
+			
+			#見つからない場合
+			if [ -z "$result" ]; then
+				echo "そのサービスは登録されていません。"
+			#見つかった場合
+			else
+				user_name=$(echo $result | cut -d':' -f2)
+				password=$(echo $result | cut -d':' -f3)
+				echo "サービス名：$service_name"
+				echo "ユーザー名：$user_name"
+				echo "パスワード：$password"
+			fi
+			;;
+		
 	esac
-done			
+done
