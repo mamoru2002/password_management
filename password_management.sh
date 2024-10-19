52deba98f8cb7a38acd902f81d15a0208307e19b
mamoru2002 <tiusika.m@gmail.com>
2024-10-19 11:27:16 +0000

    while文とcase文の追加

--- password_management.sh
+++ password_management.sh
@@ -2,12 +2,21 @@
 
 ACCOUNT="password_management_table"
 
-echo "パスワードマネージャへようこそ！"
-echo "サービス名を入力してください"
-read service
-echo "ユーザー名を入力してください"
-read user
-echo "パスワードを入力してください"
-read password
-echo "$service:$user:$password" >> $ACCOUNT
-echo "Thank you!"
+while true; do
+	echo "パスワードマネージャへようこそ！"
+	echo "次の選択肢から入力してください(Add Password)："
+	read option
+
+	case $option in
+		"Add Password")
+			echo "サービス名を入力してください"
+			read service
+			echo "ユーザー名を入力してください"
+			read user
+			echo "パスワードを入力してください"
+			read password
+			echo "$service:$user:$password" >> $ACCOUNT
+			echo "パスワードの追加は成功しました。"
+			;;
+	esac
+done			
